60baba8132ff6672281a36d7e0d51c6493d31e34
qatestermca <qatestermca@gmail.com>
2020-06-19 13:46:30 +0530

    3rd commit

--- src/Test.java
+++ src/Test.java
@@ -2,8 +2,14 @@
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int a=10;
+		int b=20;
+		int c;
+		c=a;
+		a=b;
+		b=c;
+		System.out.println("value of a after swapping="+a );
+		System.out.println("value of b after swapping="+b );
 	}
 
 }
